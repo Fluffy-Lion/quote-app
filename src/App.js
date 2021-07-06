36093e82f7731fbe1617aa8a28443af84a5efa5c
Fluffy-Lion <leoncooper31@gmail.com>
2021-07-06 14:11:54 +0100

    error handling starting point

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
 import { useState } from 'react'
+import { BrowserRouter as Router, Link, Switch } from 'react-router-dom'
 require('dotenv').config()
 
-function App() {
- return <h1>quote</h1>
+const App = () => {
+
+    const [quote, setQuote] = useState("")
+    const [error, setError] = useState({ error: false, message: "" })
+
+    const collect = async () => {
+        try{
+        const response = await fetch('https://tronalddump.io/random/quote')
+        if(response.status !== 200){
+            throw new Error("oops")
+        }
+        const data = await response.json()
+        setQuote(data.value)
+        } catch(error){
+            console.log(error)
+        }
+    }
+
+    return (
+        <div>
+            <h2>{quote}</h2>
+            <button onClick={collect}>get quote</button>
+        </div>
+    )
 }
 
 export default App;
